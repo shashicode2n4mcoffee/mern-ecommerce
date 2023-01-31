9802e6d1337189c15bf560af963134b72ad99a22
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-01-31 12:42:48 +0530

    Error Handlers added

---
+++ moddlewares/errorHandler.js
@@ -0,0 +1,12 @@
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+
+  res.status(statusCode)
+
+  res.json({
+    message: error?.message,
+    stack: error?.stack,
+  })
+}
+
+module.exports = errorHandler
