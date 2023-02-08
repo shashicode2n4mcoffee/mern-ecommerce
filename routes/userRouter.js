c37d903ce3f5b1422b4d0afc48af7a5335d2e000
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-02-09 00:30:16 +0530

    role schema , validation and route added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,9 +7,10 @@ const validate = require('../validation')
 
 const loginUserSchema = require('../validation/loginUser')
 const newUserSchema = require('../validation/newUserSchema')
+const updateRoleSchema = require('../validation/updateRoleSchema')
 
 router.post('/user', validate(newUserSchema), userCtrl.registerUser)
 router.post('/user', validate(loginUserSchema), userCtrl.loginUser)
-
+router.post('/user', validate(updateRoleSchema), userCtrl.updateRole)
 
 module.exports = router
---
+++ validation/updateRoleSchema.js
@@ -0,0 +1,10 @@
+const yup = require('yup')
+
+const updateRoleSchema = yup.object({
+  body: yup.object({
+    email: yup.string().email().required,
+    role: yup.string().required,
+  }),
+})
+
+module.exports = updateRoleSchema
