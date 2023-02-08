66e8d579bac1ad76eb3b8b9324a83a12197d7838
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-02-09 00:08:38 +0530

    Login user schema is added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,8 +3,11 @@ const router = express.Router()
 
 const { userCtrl } = require('../controllers/index')
 const validate = require('../validation')
+
+const loginUserSchema = require('../validation/loginUser')
 const newUserSchema = require('../validation/newUserSchema')
 
 router.post('/user', validate(newUserSchema), userCtrl.registerUser)
+router.post('/user', validate(loginUserSchema), userCtrl.loginUser)
 
 module.exports = router
---
+++ validation/loginUser.js
@@ -0,0 +1,10 @@
+const yup = require('yup')
+
+const loginUserSchema = yup.object({
+  body: yup.object({
+    email: yup.string().email().required,
+    password: yup.string().required().min(4).max(100),
+  }),
+})
+
+module.exports = loginUserSchema
